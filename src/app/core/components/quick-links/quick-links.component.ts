4aae6164bfb8b645b1f400e581ccd9fcbf8ac893
sergey-laschotko <anaharet@tut.by>
2020-04-06 15:55:14 +0300

    create "Use a gift card page"

--- src/app/core/components/quick-links/quick-links.component.ts
+++ src/app/core/components/quick-links/quick-links.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { DataService } from '../../../core/providers/data/data.service';
 import { BreakingChangeType } from 'graphql';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'vsf-quick-links',
@@ -9,7 +10,7 @@ import { BreakingChangeType } from 'graphql';
   styleUrls: ['./quick-links.component.scss']
 })
 export class QuickLinksComponent implements OnInit {
-
+  form: FormGroup;
   mode: any = null;
   // for test
   termAndConditions = `TERMS AND CONDITIONS OF THE BEST VOUCHERS WEBSITE
@@ -218,7 +219,6 @@ export class QuickLinksComponent implements OnInit {
   The Data Controller reserves the right to introduce changes or amendments to this Privacy Policy. Changes are made by posting new content of the Terms and Conditions on the Website. Changes become effective at the time of their introduction.
   The precondition for the use of the Website is the acceptance of this Privacy Policy.`;
 
-
   listLocation = [
     {
       loc: 'Български',
@@ -334,7 +334,18 @@ export class QuickLinksComponent implements OnInit {
    }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl('', [Validators.email, Validators.required]),
+      code: new FormControl(null, [Validators.required, Validators.maxLength(8), Validators.minLength(8)]),
+    });
+  }
 
+  submit() {
+    if (this.form.valid){
+      console.log('Form', this.form);
+      const formData = {...this.form.value};
+      console.log('formData', formData);
+    }
   }
 
 }
